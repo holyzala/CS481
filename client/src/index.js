d87a43bba5fb9f6f942a87743afdbabd2f83ba15
Christopher Magnuson <magnus32@uwm.edu>
2018-04-24 10:06:21 -0500

    Client tutorial

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter } from 'react-router-redux'
+import { Provider } from 'react-redux'
+import {Route, Switch} from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
+import Login from './containers/Login';
+import PrivateRoute from './containers/PrivateRoute';
+import configureStore from './store'
+
+const history = createHistory()
+
+const store = configureStore(history)
+
+ReactDOM.render((
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path="/login/" component={Login} />
+        <PrivateRoute path="/" component={App}/>
+      </Switch>
+    </ConnectedRouter>
+  </Provider>
+), document.getElementById('root'));
